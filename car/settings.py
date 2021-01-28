4e8167b935f5595e348706bab18f50cd68954478
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-28 11:59:51 +0300

    add database

--- car/settings.py
+++ car/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'car.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'car',
+        'USER': 'moringa',
+        'PASSWORD':'1234',
+                                
     }
 }
 
