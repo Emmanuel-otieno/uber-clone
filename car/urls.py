bff26518885e064b40b7ffca1a94c31efdce5047
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-28 14:30:53 +0300

    update my template

--- car/urls.py
+++ car/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('driver.urls')),
     path('',include('user.urls')),
+    path(r'accounts/', include('registration.backends.simple.urls')),
+    path(r'logout/', views.logout, {"next_page": '/'}), 
 ]
