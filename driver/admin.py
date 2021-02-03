8f87abedaec1b5ca3f07f113ad1a36e183d7cbce
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-03 14:07:25 +0300

    deployment

--- driver/admin.py
+++ driver/admin.py
@@ -3,6 +3,6 @@ from .models import Location,Driver,Car
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Driver)
-admin.site.register(Location)
+admin.site.register(Location) 
 # admin.site.register(Category)
 # admin.site.register(Passenger)
